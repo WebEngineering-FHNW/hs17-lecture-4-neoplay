e96bdf1452d9542e39828c227679a0f553d9204f
Urs Gürtler <u.guertler@fusion7.ch>
2017-10-24 14:41:12 +0200

    Activate in-place validation, integration test

--- src/integration-test/groovy/mvc/InPlaceCalculatorSpec.groovy
+++ src/integration-test/groovy/mvc/InPlaceCalculatorSpec.groovy
@@ -42,26 +42,24 @@ class InPlaceCalculatorSpec extends GebSpec {
             $("#exam").attr('class') == ""
     }
 
-// TODO: un-comment the commented lines below and see them failing, then make them pass
-
     void "Invalid input is handled in-place by JS without submission"() {
         given: "a valid state"
             browser.driver.javascriptEnabled = true
             go '/InPlaceCalculator.html'
             $("form").en   = 3.0
             $("form").exam = 3.0
-        when:
+        when: "erster roundtrip ist noch mit klick"
             $("input", type: "submit").click()
         then: "we should have a clean, valid state to start from"
             $("#en").attr('class') == ""
         when: "we enter some invalid value _without_ submitting"
-//            def message = withAlert {
+            def message = withAlert {
                 $("form").en = 0.9
-//            }
+            }
         then: "the in-place JS logic should kick in"
-//            $("#en").attr('class') == "error"
-//            message == "en value needs to be at least 1.0"
-//            $("#en").focused
+            //$("#en").attr('class') == "error"
+            message == "en value needs to be at least 1.0"
+            $("#en").focused
     }
 
 
